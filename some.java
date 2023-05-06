72e0d245466284e6f5f1a53a1513773f138096ce
aditya9264 <aditya9919494435@gmail.com>
2023-05-06 13:19:47 +0530

    added message in some.java

--- some.java
+++ some.java
@@ -2,6 +2,6 @@ public class some{
     public static void main(String[] args) {
         int a=5;
         int b=6;
-        System.out.println("The Sum is - "+ (a+b));
+        System.out.println("The Sum of two numbers is - "+ (a+b));
     }
 }
